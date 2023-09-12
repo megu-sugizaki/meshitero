d1d8cefabe337e9fe5c857dbe47890636154dc11
EC2 Default User <ec2-user@ip-172-31-9-47.ap-northeast-1.compute.internal>
2023-09-12 04:24:13 +0000

    ページネーション途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+ before_action :authenticate_user!, except: [:top]
+ before_action :configure_permitted_parameters, if: :devise_controller?
+ #devise利用の機能（ユーザ登録、ログイン認証等）が使われる前にconfigure_permitted_parametersメソッドが実行される。
+ 
+ def after_sign_in_path_for(resource)
+  poar_imagespath
+ end
+ 
+ def after_sign_out_path_for(resource)
+  about_path
+ end 
+ 
+ protected 
+ #strong parameterと違い、呼び出された他のコントローラーからも参照可能。
+
+ def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+     #ユーザー登録の際に新たな追加機能のユーザー名のデータ操作を許可。strong parameterと同様、記述をしたコントローラー内でしか操作できない。
+ end 
 end
