7d04ed7af854437a1a31a093b9c27d7ceaee6575
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-12-28 01:47:31 +0000

    [Fix]before_action deleted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
- before_action :authenticate_user!, except: [:top]
+ # before_action :authenticate_user!, except: [:top]
  before_action :configure_permitted_parameters, if: :devise_controller?
  #devise利用の機能（ユーザ登録、ログイン認証等）が使われる前にconfigure_permitted_parametersメソッドが実行される。
  
